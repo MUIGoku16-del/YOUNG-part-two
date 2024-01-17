5eb8c4d240e547e9dc6a1b9130a3c5275a681c1e
Christopher Young <cleon1224@gmail.com>
2024-01-17 10:19:56 -0500

    'git reset --soft HEAD~1

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 let name = prompt("Welcome to GC mini golf! What is your name?");
-let golfHoles = prompt("Hi, " + name + "! Would you like to play 3 or 6 holes today?");
+let golfHoles = prompt(
+  "Hi, " + name + "! Would you like to play 3 or 6 holes today?"
+);
 let total = 0;
-let par = golfHoles * Number (3);
+let par = golfHoles * 3;
 for (let i = 0; i < golfHoles; i++) {
-    let score = Number( prompt("How many putts for hole " + (i+1) + "? (par is 3)") );
-    total += score;
+  let score = Number(
+    prompt("How many putts for hole " + (i + 1) + "? (par is 3)")
+  );
+  total += score;
 }
 
-if (total === par) {
-    console.log ("Good game, " + name + ". Your total par was: " + total + ".");
-}
+let scoreDiff = total - par;
 
-else if (total < par) {
-    console.log ("Great job, " + name + "! Your total par was: - " + total + ".");
-}
-
-else if (total > par) {
-    console.log ("Nice try " + name + ". Your total par was: + " + total + ".");
+if (scoreDiff === 0) {
+  console.log(
+    "Good game, " + name + ". Your total par was: " + scoreDiff + "."
+  );
+} else if (scoreDiff < 0) {
+  console.log(
+    "Great job, " + name + "! Your total par was: - " + scoreDiff + "."
+  );
+} else if (scoreDiff > 0) {
+  console.log(
+    "Nice try " + name + ". Your total par was: + " + scoreDiff + "."
+  );
 }
